4d61b8f3970248613af5a46ff9c77ecfedaa65a5
vincentdeaugustine <vinnied44@csu.fullerton.edu>
2020-04-22 14:49:08 -0700

    update FTPPrompt

--- Python/FTPPrompt.py
+++ Python/FTPPrompt.py
@@ -1,35 +1,49 @@
-userInput = input("ftp>")
+from socket import *
 
-if userInput.startswith('ls'):
-    print("Now printing out the directory")
+serverPort = 12000
 
-elif userInput.lower().startswith('get '):
-    pass
-    # Check if the file exists in the server
+while True:
 
-    # If the file does not exist in the server:
-        # print("Cannot retrieve the file from the server."
-        # break
-    # else if the file does exist in the server:
-        # download the file from the server
-        # print("File successfully downloaded."
+    userInput = raw_input("ftp>")
 
-elif userInput.lower().startswith('put '):
-    pass
-    # Check if the file exists in the server
 
-    # If the file already exists in the server:
-        # print("Another file already exists in the server."
-        # break
+    if userInput.startswith('ls'):
+        print("Now printing out the directory")
 
-    # else if the file does not already exists in the server:
-        # Upload the file to the server
-        # print("File successfully uploaded")
+    elif userInput.lower().startswith('get '):
+        print("User Entered 'get' command")
 
-elif userInput.lower().startswith('quit'):
-    print("Disconnecting from server.")
-    # break out of while loop
+        pass
+        # Check if the file exists in the server
 
-else:
-    print("Invalid command.")
+        # If the file does not exist in the server:
+            # print("Cannot retrieve the file from the server."
+            # break
+        # else if the file does exist in the server:
+            # download the file from the server
+            # print("File successfully downloaded."
+
+    elif userInput.lower().startswith('put '):
+        print("User Entered 'put' command")
+
+        pass
+        # Check if the file exists in the server
+
+        # If the file already exists in the server:
+            # print("Another file already exists in the server."
+            # break
+
+        # else if the file does not already exists in the server:
+            # Upload the file to the server
+            # print("File successfully uploaded")
+
+    elif userInput.lower().startswith('quit'):
+        print("User Entered 'quit' command")
+        print("Disconnecting from server.")
+        # break out of while loop
+        break
+
+    else:
+        print("Invalid command.")
+
 
